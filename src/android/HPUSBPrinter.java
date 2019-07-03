764464f59073b2744ec23bbdfafa8831a0d498fd
Jacob Ntesang <ntesangjacob@gmail.com>
2019-07-03 10:55:23 +0200

    I have added the HPUSBPrinter.java file and implemented the following methods:
    - setUp(): Sets up the printer according to the HP SDK instructions, which are open, claim and enable the printer.
    - print(): Takes a print line as it is and prints it out. If the line has any font properties, it should be sent with those properties included in the line. This will only print out the line and not check for any special properties.
    - cut(): Cuts the paper after feeding 6 printLines.
    - disconnect(): Release resources and disconnect the printer.
    
    I have also created a bridge file(HPUSBPrinter.js) to connect the native side to JS.

---
+++ src/android/HPUSBPrinter.java
@@ -0,0 +1,107 @@
+package com.jacob;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.json.JSONArray;
+import org.json.JSONException;
+import jpos.JPOSApp;
+import jpos.JposException;
+import jpos.POSPrinter;
+import jpos.POSPrinterConst;
+import android.content.Context;
+
+import com.hp.android.possdk.IJPOSInitCompleteCallBack;
+
+
+public class HPUSBPrinter extends CordovaPlugin implements IJPOSInitCompleteCallBack {
+
+    @Override
+    public void onComplete() { // IJPOSInitCompleteCallBack callback for SDK initialization complete
+
+    }
+
+    POSPrinter printer = new POSPrinter();
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        JPOSApp.setsContext(webView.getContext(),(IJPOSInitCompleteCallBack)this);
+
+        if ("setup".equals(action)) {
+            // Set-up the printer for use.
+            setup(callbackContext);
+            return true;
+        }
+        else if ("print".equals(action)) {
+            // Print the line.
+            print(args.getString(0), callbackContext);
+            return true;
+        }
+        else if ("printLine".equals(action)) {
+            // Print the line.
+            print(args.getString(0), callbackContext);
+            return true;
+        }
+        else if ("cut".equals(action)) {
+            // Cut paper.
+            cut(args.getString(0), callbackContext);
+            return true;
+        }
+        else if ("disconnect".equals(action)) {
+            // Disconnect the printer.
+            disconnect(callbackContext);
+            return true;
+        }
+        return false;
+    };
+
+    private void setup(CallbackContext callbackContext) {
+        // A device must be open, claim, and DeviceEnable=true for it to be used.
+
+        try {
+            printer.open("HPEngageOnePrimePrinter"); // HP Engage One Prime White Receipt Printer
+            printer.claim(1000);
+            printer.setDeviceEnabled(true);
+        } catch (JposException e) {
+            callbackContext.error(e.getLocalizedMessage().toString());
+        }
+
+    };
+
+    private void print(String msg, CallbackContext callbackContext) {
+        // Print
+        try {
+            printer.printNormal(POSPrinterConst.PTR_S_RECEIPT, msg);
+            callbackContext.success("Success!");
+        } catch (JposException e) {
+            callbackContext.error(e.getLocalizedMessage().toString());
+        }
+
+    };
+
+    private void cut(String msg, CallbackContext callbackContext) {
+        // Feed 6-lines(msg) before cutting paper.
+
+        try {
+            printer.printNormal(POSPrinterConst.PTR_S_RECEIPT, msg);
+            printer.cutPaper(100);
+            callbackContext.success("Success!");
+        } catch (JposException e) {
+            callbackContext.error(e.getLocalizedMessage().toString());
+        }
+
+    };
+
+    private void disconnect(CallbackContext callbackContext) {
+        // Disconnect printer, but first do some resource management and disable the printer.
+
+        try {
+            printer.setDeviceEnabled(false);
+            printer.release();
+            printer.close();
+            callbackContext.success("Success!");
+        } catch (JposException e) {
+            callbackContext.error(e.getLocalizedMessage().toString());
+        }
+
+    };
+
+}
---
+++ www/HPUSBPrinter.js
@@ -0,0 +1,34 @@
+
+var exec = require('cordova/exec');
+
+var USBPrinter = {
+    //A device must be open, claim, and DeviceEnable=true for it to be used.
+    setup: function (msg, successCallback, errorCallback) {
+        console.log('HPUSBPrinter setup method invoked');
+        exec(successCallback, errorCallback, "HPUSBPrinter", "setup", [msg]);
+    },
+    // Sent printJob.
+    print: function (msg, successCallback, errorCallback) {
+        console.log('HPUSBPrinter setup method invoked');
+        exec(successCallback, errorCallback, "HPUSBPrinter", "print", [msg]);
+    },
+
+    // Print a new line.
+    println: function (msg, successCallback, errorCallback) {
+        console.log('HPUSBPrinter setup method invoked');
+        exec(successCallback, errorCallback, "HPUSBPrinter", "printLine", [msg]);
+    },
+
+    // Cut Paper
+    cutPaper: function (msg, successCallback, errorCallback) {
+        console.log('HPUSBPrinter setup method invoked');
+        exec(successCallback, errorCallback, "HPUSBPrinter", "cut", [msg]);
+    },
+
+    disconnect: function (msg, successCallback, errorCallback) {
+        console.log('HPUSBPrinter setup method invoked');
+        exec(successCallback, errorCallback, "HPUSBPrinter", "disconnect", [msg]);
+    }
+};
+
+module.exports = USBPrinter;
